dbb5b7b803cfc627cd48da56b7976a35b5a69992
Bruno Barbosa <brunobm19992014@gmail.com>
2022-11-25 16:55:34 +0000

    IProductRepository implementation

--- DemoShopping.ProductAPI/Data/Repository/IProductRepository.cs
+++ DemoShopping.ProductAPI/Data/Repository/IProductRepository.cs
@@ -1,5 +1,11 @@
-﻿namespace DemoShopping.ProductAPI.Data.Repository {
-    public interface IProductRepository {
+﻿using DemoShopping.ProductAPI.Entities.Value_Objects;
 
+namespace DemoShopping.ProductAPI.Data.Repository {
+    public interface IProductRepository {
+        Task<IEnumerable<ProductVO>> FindAll();
+        Task<ProductVO> FindById(long id);
+        Task<ProductVO> Create(ProductVO vo);
+        Task<ProductVO> Update(ProductVO vo);
+        Task<bool> Delete(long id);
     }
 }
